bc5db9cc87ba1a0ee59bffa2f1770fbfee281f55
tatsukiono <64731027+tatsukiono@users.noreply.github.com>
2020-06-25 17:24:09 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.14.1'
 # 自身のアプリ名、リポジトリ名を記述
 set :application, 'Great_happiness'
 set :repo_url,  'git@github.com:tatsukiono
-/Great_happiness'
+/Great_happiness.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
